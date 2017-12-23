71d7035f65394c9412c6ee8e5aa300ad9704e24d
Rojigan Gengatharan <rojigangengatharan@Rojigans-MacBook-Pro.local>
2017-12-23 01:40:12 -0500

    basic reading of mp3 metadata working

--- mp3_lyric_tag.py
+++ mp3_lyric_tag.py
@@ -1,4 +1,15 @@
 ## Author: Rojigan Gengatharan
 ## mp3_lyric_tag.py
 ## December 23, 2017.
+## make the final program an executable and not a python script (but you can inc##  that too, can try using pyinstaller)
 
+
+import eyed3
+import os 
+
+os.chdir("test_music")
+
+audiofile = eyed3.load("favorite_song.mp3")
+
+print(audiofile.tag.artist)
+print(audiofile.tag.album)
