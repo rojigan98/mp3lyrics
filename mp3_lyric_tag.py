f6344e6222b45b60cde9406bbc39737edc113be8
Rojigan Gengatharan <rojigangengatharan@Rojigans-MacBook-Pro.local>
2017-12-23 19:46:20 -0500

    tested saving metadata apart from lyrics, seems to work

--- mp3_lyric_tag.py
+++ mp3_lyric_tag.py
@@ -2,6 +2,7 @@
 ## mp3_lyric_tag.py
 ## December 23, 2017.
 ## make the final program an executable and not a python script (but you can inc##  that too, can try using pyinstaller)
+## make it so that user decides what name of folder to receive lyrics for 
 
 
 import eyed3
@@ -9,7 +10,10 @@ import os
 
 os.chdir("test_music")
 
-audiofile = eyed3.load("favorite_song.mp3")
 
-print(audiofile.tag.artist)
-print(audiofile.tag.album)
+
+
+
+audiofile = eyed3.load("ultralight_beam.mp3")
+
+## use audiofile.save() to save changes 
